ca33daab1460d30b77eccaa73432a8131e412924
Buehler <Steven.Buehler@PMSI.CORP>
2019-06-19 11:59:14 -0400

    Remove entire site.

--- _plugins/strip_newlines.rb
+++
@@ -1,8 +0,0 @@
-module Jekyll
-	  module NewLinesFilter
-			def strip_newlines_with_space(input)
-				input.to_s.gsub(/\n+/, ' ')
-			end
-		end
-end
-Liquid::Template.register_filter(Jekyll::NewLinesFilter)
